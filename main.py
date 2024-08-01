801891fca56f956a5c1d1ddba98c46b87f74570d
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-07-31 22:45:41 -0300

    Update main to perform a simple menu

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from diff import Diff
 
-for i in range(1,21):
+while True:
+    op = input('Do you want to handle changes for all files or just for one file? (all/one/stop): ')
 
-    changes = Diff(f'source/{i}.txt')
-    changes.save_file(f'new_source/{i}.txt')
+    if op == 'stop':
+        break
+
+    if op == 'one':
+        file = input('Enter the file number: ')
+        changes = Diff(f'source/{file}.txt')
+        changes.show_together()
+        changes.save_file(f'new_source/{file}.txt')
+    else :
+        for i in range(1,21):
+
+            changes = Diff(f'source/{i}.txt')
+            changes.save_file(f'new_source/{i}.txt')
